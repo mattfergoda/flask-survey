9bc7384661f40e61208ab8c5b629e30c2c66ba4c
Logan Winnie <loganwinnier@gmail.com>
2023-09-28 14:54:10 -0400

    WIP: Step Six

--- app.py
+++ app.py
@@ -10,17 +10,32 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
+
 @app.get("/")
 def render_home_page():
     return render_template("survey_start.html", survey=survey)
+
 
 @app.post("/begin")
 def begin_page_redirect():
     return redirect('/question/0')
 
+
+@app.post("/<question_id>/answer")
+def anwser_page_redirect(question_id):
+
+    responses.append(request.form["answer"])
+
+    return redirect(f"/question/{question_id}")
+
+
 @app.get("/question/<int:question_id>")
 def question_page(question_id):
+    question_id += 1
+
     if question_id < len(survey.questions):
-        return render_template("question.html", question = survey.questions[question_id])
+
+        return render_template("question.html", question=survey.questions[question_id], question_id=question_id)
     else:
+
         return render_template("completion.html")
