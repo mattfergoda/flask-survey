a898565dda29d19f6cbbb8446fd6116fabfad3b2
Matt Fergoda <mattfergoda@users.noreply.github.com>
2023-09-28 15:57:28 -0700

    Finished step 7.

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
-responses = []
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
@@ -23,7 +21,7 @@ def begin_page_redirect():
     """Redirects from the '/begin' endpoint to question 0"""
 
     """ global responses """
-    responses.clear()
+    session['responses'] = []
 
     return redirect('/question/0')
 
@@ -33,22 +31,35 @@ def anwser_page_redirect():
     """takes a question_id. Routes to anwser page for question. Appends
     response to response list. Redirects to next question"""
 
+    responses = session['responses']
     responses.append(request.form["answer"])
+    session['responses'] = responses
 
-    return redirect(f"/question/{len(responses)}")
+    return redirect(f"/question/{len(session['responses'])}")
 
 
 @app.get("/question/<int:question_id>")
 def question_page(question_id):
     """Takes a question_id. Renders either next question or completion page."""
 
-    if question_id < len(survey.questions):
-
-        return render_template("question.html", question=survey.questions[question_id])
-    else:
-        # TODO: Redirect instead.
+    next_question_id = len(session['responses'])
+    if next_question_id == len(survey.questions):
         return redirect("/complete")
+    elif question_id != next_question_id:
+        return redirect(f"/question/{next_question_id}")
+    else:
+        return render_template("question.html", question=survey.questions[next_question_id])
+
 
 @app.get("/complete")
 def completion_page():
-    return render_template("completion.html", survey=survey, responses=responses)
+    """
+    If the user has answered all the questions, render the completion page.
+    Otherwise, redirect to the next question.
+    """
+
+    next_question_id = len(session['responses'])
+    if next_question_id == len(survey.questions):
+        return render_template("completion.html", survey=survey)
+    else:
+        return redirect(f"/question/{next_question_id}")
