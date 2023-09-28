1754e282f86849de143c51fb78b508e646beed6d
Logan Winnie <loganwinnier@gmail.com>
2023-09-28 17:56:47 -0400

    fixed part 5

--- app.py
+++ app.py
@@ -13,29 +13,38 @@ debug = DebugToolbarExtension(app)
 
 @app.get("/")
 def render_home_page():
+    """Renders Home page"""
+
     return render_template("survey_start.html", survey=survey)
 
 
 @app.post("/begin")
 def begin_page_redirect():
+    """Redirects from the '/begin' endpoint to question 0"""
+
+    """ global responses """
+    responses.clear()
+
     return redirect('/question/0')
 
 
-@app.post("/<question_id>/answer")
-def anwser_page_redirect(question_id):
+@app.post("/answer")
+def anwser_page_redirect():
+    """takes a question_id. Routes to anwser page for question. Appends
+    response to response list. Redirects to next question"""
 
     responses.append(request.form["answer"])
 
-    return redirect(f"/question/{question_id}")
+    return redirect(f"/question/{len(responses)}")
 
 
 @app.get("/question/<int:question_id>")
 def question_page(question_id):
-    question_id += 1
+    """Takes a question_id. Renders either next question or completion page."""
 
     if question_id < len(survey.questions):
 
-        return render_template("question.html", question=survey.questions[question_id], question_id=question_id)
+        return render_template("question.html", question=survey.questions[question_id], question_id=question_id+1)
     else:
 
         return render_template("completion.html", survey=survey, responses=responses)
