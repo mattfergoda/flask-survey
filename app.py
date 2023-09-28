a29588dd6c3cf369d3cd71b4a26f9af2bc514cb8
Logan Winnie <loganwinnier@gmail.com>
2023-09-28 19:42:07 -0400

    Step 8 complete

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+SESSION_KEY = "responses"
 
 
 @app.get("/")
@@ -17,38 +18,38 @@ def render_home_page():
 
 
 @app.post("/begin")
-def begin_page_redirect():
-    """Redirects from the '/begin' endpoint to question 0"""
+def begin_survey():
+    """Clears session responses to begin survey.
+    Redirects from the '/begin' endpoint to question 0"""
 
-    """ global responses """
-    session['responses'] = []
+    session[SESSION_KEY] = []
 
     return redirect('/question/0')
 
 
 @app.post("/answer")
-def anwser_page_redirect():
+def handle_question():
     """takes a question_id. Routes to anwser page for question. Appends
     response to response list. Redirects to next question"""
 
-    responses = session['responses']
+    responses = session[SESSION_KEY]
     responses.append(request.form["answer"])
-    session['responses'] = responses
+    session[SESSION_KEY] = responses
 
-    return redirect(f"/question/{len(session['responses'])}")
+    return redirect(f"/question/{len(session[SESSION_KEY])}")
 
 
 @app.get("/question/<int:question_id>")
 def question_page(question_id):
     """Takes a question_id. Renders either next question or completion page."""
-
-    next_question_id = len(session['responses'])
+    next_question_id = len(session[SESSION_KEY])
     if next_question_id == len(survey.questions):
         return redirect("/complete")
     elif question_id != next_question_id:
+        flash("ANSWER THEM IN ORDER")
         return redirect(f"/question/{next_question_id}")
-    else:
-        return render_template("question.html", question=survey.questions[next_question_id])
+
+    return render_template("question.html", question=survey.questions[next_question_id])
 
 
 @app.get("/complete")
@@ -58,8 +59,9 @@ def completion_page():
     Otherwise, redirect to the next question.
     """
 
-    next_question_id = len(session['responses'])
-    if next_question_id == len(survey.questions):
-        return render_template("completion.html", survey=survey)
-    else:
+    next_question_id = len(session[SESSION_KEY])
+    # TODO: Swap Logic
+    if next_question_id != len(survey.questions):
         return redirect(f"/question/{next_question_id}")
+
+    return render_template("completion.html", survey=survey)
