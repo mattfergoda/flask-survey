7ac9093a995d518bf78b7c6bb4e383424f90f24a
Matt Fergoda <mattfergoda@users.noreply.github.com>
2023-09-28 11:32:30 -0700

    WIP step 3.

--- app.py
+++ app.py
@@ -2,8 +2,25 @@ from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
+responses = []
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+@app.get("/")
+def render_home_page():
+    return render_template("survey_start.html", survey=survey)
+
+@app.post("/begin")
+def begin_page_redirect():
+    return redirect('/question/0')
+
+@app.get("/question/<int:question_id>")
+def question_page(question_id):
+    if question_id < len(survey.questions):
+        return render_template("question.html", question = survey.questions[question_id])
+    else:
+        return render_template("completion.html")
